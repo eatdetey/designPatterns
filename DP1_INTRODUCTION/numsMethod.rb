80812377ca7ade32b5ba22ed3bd6e1da5018e313
pawa andreevna <80104744+eatdetey@users.noreply.github.com>
2024-09-13 19:51:21 +0300

    Task 4 All Methods

--- DP1_INTRODUCTION/numsMethod.rb
+++ DP1_INTRODUCTION/numsMethod.rb
@@ -44,5 +44,29 @@ def countDigitsGreaterThenThree(num)
 	return count
 end
 
+#Method 3 (Метод 3 Найти произведение таких делителей числа, сумма цифр
+#которых меньше, чем сумма цифр исходного числа.)
+def digitsSum(num)
+	sum = 0
+	until num == 0 do
+		sum += num % 10
+		num /= 10
+	end
+	return sum
+end
+
+def dividersProduct(num)
+	dSum = digitsSum(num)
+	product = 1
+	for i in 2..num/2 do
+		if num % i == 0 and dSum > digitsSum(i) then
+			product *= i
+		end
+	end
+	return product
+end
+
+
 puts(dividersSum(ARGV[0].to_i))
 puts(countDigitsGreaterThenThree(ARGV[1].to_i))
+puts(dividersProduct(ARGV[2].to_i))

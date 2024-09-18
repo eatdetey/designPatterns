2ecae87a6b594d33442ec69d1112466a20fa2be0
pawa andreevna <80104744+eatdetey@users.noreply.github.com>
2024-09-19 01:09:17 +0300

    Task 5&6
    Changes in main.rb in accordance with changes in class structure

--- DP2_CLASSES/main.rb
+++ DP2_CLASSES/main.rb
@@ -1,8 +1,8 @@
 require './classStudent.rb'
 
-student_ivan = Student.new("Ivanov", "Ivan", "Ivanovich", 1, "+79609998877", "@ivanich", "ivanich@mail.ru", "github.com/ivanich")
-student_petr = Student.new("Petrov", "Petr", "Petrovich", nil, nil, "@petrucho")
-student_alex = Student.new("Aleksandrov", "Aleksandr", "Aleksandrovich", 2, nil, nil, "alex2014@yandex.ru", "github.com/lionalex")
+student_ivan = Student.new("Ivanov", "Ivan", "Ivanovich", id:1, phone_num:"+79609998877", telegram:"@ivanich", email:"ivanich@mail.ru", git:"github.com/ivanich")
+student_petr = Student.new("Petrov", "Petr", "Petrovich", telegram:"@petrucho")
+student_alex = Student.new("Aleksandrov", "Aleksandr", "Aleksandrovich", id:2, email:"alex2014@yandex.ru", git:"github.com/lionalex")
 
 student_ivan.show_info
 student_petr.show_info

2edcab757f464a7add4a145879c2306777dbd634
pawa andreevna <80104744+eatdetey@users.noreply.github.com>
2024-09-19 22:08:08 +0300

    Task 7
    Added more data to demostrate phone number validation

--- DP2_CLASSES/main.rb
+++ DP2_CLASSES/main.rb
@@ -1,9 +1,12 @@
 require './classStudent.rb'
 
-student_ivan = Student.new("Ivanov", "Ivan", "Ivanovich", id:1, phone_num:"+79609998877", telegram:"@ivanich", email:"ivanich@mail.ru", git:"github.com/ivanich")
-student_petr = Student.new("Petrov", "Petr", "Petrovich", telegram:"@petrucho")
-student_alex = Student.new("Aleksandrov", "Aleksandr", "Aleksandrovich", id:2, email:"alex2014@yandex.ru", git:"github.com/lionalex")
-
+student_ivan = Student.new("Ivanov", "Ivan", "Ivanovich", id:1, phone_num:"+79509995552", telegram:"@ivanich", email:"ivanich@mail.ru", git:"github.com/ivanich")
 student_ivan.show_info
+
+student_petr = Student.new("Petrov", "Petr", "Petrovich", phone_num:"aaaavtomobil", telegram:"@petrucho")
 student_petr.show_info
+
+student_alex = Student.new("Aleksandrov", "Aleksandr", "Aleksandrovich", phone_num:"9605553535", id:2, email:"alex2014@yandex.ru", git:"github.com/lionalex")
 student_alex.show_info
+
+

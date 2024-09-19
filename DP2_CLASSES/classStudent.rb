47291eed6ee2d7a0be757045373b316814f4a807
pawa andreevna <80104744+eatdetey@users.noreply.github.com>
2024-09-20 00:00:17 +0300

    Task 8
    Validate everything!

--- DP2_CLASSES/classStudent.rb
+++ DP2_CLASSES/classStudent.rb
@@ -1,22 +1,134 @@
 class Student
 	def initialize (surname, name, patronymic, **contacts)
-		@surname = surname
-		@name = name
-		@patronymic = patronymic
-		@id = contacts[:id]
-		self.phone_num = contacts[:phone_num] ##Change instance variable(@) to self so setter with validation can work
-		@telegram = contacts[:telegram]
-		@email = contacts[:email]
-		@git = contacts[:git]
+		self.surname = surname
+		self.name = name
+		self.patronymic = patronymic
+		self.id = contacts[:id]
+		self.phone_num = contacts[:phone_num]
+		self.telegram = contacts[:telegram]
+		self.email = contacts[:email]
+		self.git = contacts[:git]
 	end
 	
-	##Getter&Setter for everything EXEPT phone number
+	## Name validator
 	
-	attr_accessor :name, :surname, :patronymic, :id, :telegram, :email, :git
+	def self.is_valid_name?(name)
+		!!(name =~ /^[А-ЯA-Z][а-яa-zA-ZА-Я\-]{1,20}$/)
+	end
 	
-	## Phone number getter
+	## ID validator
 	
-	attr_reader :phone_num
+	def self.is_valid_id?(id)
+		!!(id == nil || id.to_s =~ /^(\d+)$/)
+	end
+	
+	## Phone number validator
+	
+	def self.is_valid_phone_num?(phone_num)
+		!!(phone_num == nil || phone_num =~ /^(?:\+7|8)\d{10}$/)
+	end
+	
+	## Telegram validator
+	
+	def self.is_valid_telegram?(telegram)
+		!!(telegram == nil || telegram =~ /^@[\w]{3,20}$/)
+	end
+	
+	## Email validator
+	
+	def self.is_valid_email?(email)
+		!!(email == nil || email =~ /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+	end
+	
+	## GitHub validator
+	
+	def self.is_valid_git?(git)
+		!!(git == nil || git =~ /^https?:\/\/(www\.)?github\.com\/[A-Za-z0-9_-]+(\/[A-Za-z0-9._-]+)?(\/.*)?$/)
+	end
+
+	##Getter for everything
+	
+	attr_reader :name, :surname, :patronymic, :id, :phone_num, :telegram, :email, :git
+	
+	## Name setter
+	
+	def name=(name)
+		if self.class.is_valid_name?(name)
+			@name = name
+		else
+			raise ArgumentError, "\n Name (#{name}) has the wrong format! Object was not created."
+		end
+	end
+	
+	## Surname setter
+	
+	def surname=(surname)
+		if self.class.is_valid_name?(surname)
+			@surname = surname
+		else
+			raise ArgumentError, "\n Surname (#{surname}) has the wrong format! Object was not created."
+		end
+	end
+	
+	## Patronymic setter
+	
+	def patronymic=(patronymic)
+		if self.class.is_valid_name?(patronymic)
+			@patronymic = patronymic
+		else
+			raise ArgumentError, "\n Patronymic (#{patronymic}) has the wrong format! Object was not created."
+		end
+	end
+	
+	## Phone number setter
+	
+	def phone_num=(phone_num)
+		if self.class.is_valid_phone_num?(phone_num)
+			@phone_num = phone_num
+		else
+		puts "\nPhone number (#{phone_num}) is in the wrong format and was not recorded!"
+		end
+	end
+	
+	## ID setter
+	
+	def id=(id)
+		if self.class.is_valid_id?(id)
+			@id = id
+		else
+		puts "\nID (#{id}) is in the wrong format and was not recorded!"
+		end
+	end
+	
+	## Telegram setter
+	
+	def telegram=(telegram)
+		if self.class.is_valid_telegram?(telegram)
+			@telegram = telegram
+		else
+		puts "\nTelegram  (#{telegram}) is in the wrong format and was not recorded!"
+		end
+	end
+	
+	## Email setter
+	
+	def email=(email)
+		if self.class.is_valid_email?(email)
+			@email = email
+		else
+		puts "\nEmail (#{email}) is in the wrong format and was not recorded!"
+		end
+	end
+	
+	## GitHub setter
+	
+	def git=(git)
+		if self.class.is_valid_git?(git)
+			@git = git
+		else
+		puts "\nGithub (#{git}) link is in the wrong format and was not recorded!"
+		end
+	end
 	
 	## Method to show info about object
 	
@@ -27,21 +139,5 @@ class Student
 		telegram ? (puts "Telegram - #{@telegram}") : nil
 		email ? (puts "Email - #{@email}") : nil
 		git ? (puts "Github - #{@git}\n") : nil
-	end
-	
-	## Phone number validator
-	
-	def self.is_valid_phone_num?(phone_num)
-		!!(phone_num == nil || phone_num =~ /^(?:\+7|8)\d{10}$/)
-	end
-	
-	## Phone number setter
-	
-	def phone_num=(phone_num)
-		if self.class.is_valid_phone_num?(phone_num)
-			@phone_num = phone_num
-		else
-		puts "\nSome of the phone numbers are in the wrong format and were not recorded!"
-		end
 	end
 end
--- DP2_CLASSES/main.rb
+++ DP2_CLASSES/main.rb
@@ -1,12 +1,18 @@
 require './classStudent.rb'
 
-student_ivan = Student.new("Ivanov", "Ivan", "Ivanovich", id:1, phone_num:"+79509995552", telegram:"@ivanich", email:"ivanich@mail.ru", git:"github.com/ivanich")
-student_ivan.show_info
+begin
+student_ivan = Student.new("Ivanov", "Ivan-Ivan", "Ivanovich", id:1, phone_num:"+79509995552", telegram:"@ivanich", email:"ivanich@mail.ru", git:"https://github.com/ivanich")
+rescue ArgumentError => e
+	puts "#{e.message}"
+end
 
-student_petr = Student.new("Petrov", "Petr", "Petrovich", phone_num:"aaaavtomobil", telegram:"@petrucho")
-student_petr.show_info
+student_ivan&.show_info #& - save navigation operator (if object is nil method will not be complited
 
-student_alex = Student.new("Aleksandrov", "Aleksandr", "Aleksandrovich", phone_num:"9605553535", id:2, email:"alex2014@yandex.ru", git:"github.com/lionalex")
-student_alex.show_info
+# student_petr = Student.new("Petrov", "Petr", "Petrovich", phone_num:"aaaavtomobil", telegram:"@petrucho")
+# student_petr.show_info
+
+# student_alex = Student.new("Aleksandrov", "Aleksandr", "Aleksandrovich", phone_num:"9605553535", id:2, email:"alex2014@yandex.ru", git:"github.com/lionalex")
+# student_alex.show_info
+
 
 

cf46b96548a76b0789bfad77512846d60c3bcbad
pawa andreevna <80104744+eatdetey@users.noreply.github.com>
2024-10-19 01:32:10 +0300

    L3 T1 Final

--- Blocks, collections and tests/array manipulation/array_methods.rb
+++ Blocks, collections and tests/array manipulation/array_methods.rb
@@ -1,3 +1,17 @@
+def menu
+	puts "\nСписок задач, доступных к решению:"
+	puts "Задача 1 - Циклический сдвиг элементов массива вправо на две позиции;"
+	puts "Задача 2 - Циклический сдвиг элементов массива вправо на одну позицию;"
+	puts "Задача 3 - Поиск количества чётных элементов;"
+	puts "Задача 4 - Поиск количества минимальных элементов;"
+	puts "Задача 5 - Построение списка, упорядоченного по количеству вхождений элементов, на основе данного."
+	print "\nВведите номер задачи, которую необходимо решить, или -1, чтобы выйти из программы: "
+end
+
+def read_array_from_file(filename)
+  File.read(filename).split.map(&:to_i)
+end
+
 def shift_right(arr, i)
   arr.rotate(-i)
 end
@@ -18,10 +32,30 @@ def sort_by_frequency(arr)
   return sorted_array
 end
 
-arr = [-1, -1, -1, 2, 3, 4, 5]
-puts "Массив: #{arr}"
-puts "Задача 1 - #{shift_right(arr, 2)}"
-puts "Задача 2 - #{shift_right(arr, 1)}"
-puts "Задача 3 - #{count_even_elements(arr)}"
-puts "Задача 4 - #{count_min_elements(arr)}"
-puts "Задача 5 - #{sort_by_frequency(arr)}"
+
+loop do
+	puts "\n----------------------------------------\n"
+
+	arr = read_array_from_file('array_example.txt')
+	
+	menu
+	task_number = gets.chomp.to_i
+	
+	if task_number == -1 then break end
+	
+	puts "Массив: #{arr}"
+	case task_number
+	when 1
+		puts "Результат обработки: #{shift_right(arr, 2)}"
+	when 2
+		puts "Результат обработки: #{shift_right(arr, 1)}"
+	when 3
+		puts "Результат обработки: #{count_even_elements(arr)}"
+	when 4
+		puts "Результат обработки: #{count_min_elements(arr)}"
+	when 5
+		puts "Результат обработки: #{sort_by_frequency(arr)}"
+	else
+		puts "Задачи с таким номером не существует :("
+	end
+end

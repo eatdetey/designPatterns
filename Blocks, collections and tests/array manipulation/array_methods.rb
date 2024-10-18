c4107116e931a122f386566878343ca0faa58d45
pawa andreevna <80104744+eatdetey@users.noreply.github.com>
2024-10-19 01:10:10 +0300

    L3 T1.4 Count min elements

--- Blocks, collections and tests/array manipulation/array_methods.rb
+++ Blocks, collections and tests/array manipulation/array_methods.rb
@@ -6,8 +6,14 @@ def count_even_elements(arr)
   arr.count { |x| x.even? }
 end
 
-arr = [1, 2, 3, 4, 5]
+def count_min_elements(arr)
+  min = arr.min
+  arr.count(min)
+end
+
+arr = [-1, -1, -1, 2, 3, 4, 5]
 puts "Массив: #{arr}"
 puts "Задача 1 - #{shift_right(arr, 2)}"
 puts "Задача 2 - #{shift_right(arr, 1)}"
 puts "Задача 3 - #{count_even_elements(arr)}"
+puts "Задача 4 - #{count_min_elements(arr)}"

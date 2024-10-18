aead66cfd400174f80234b4c3b7a792cbdc544c4
pawa andreevna <80104744+eatdetey@users.noreply.github.com>
2024-10-19 01:18:54 +0300

    L3 T1.5 Sort by frequency

--- Blocks, collections and tests/array manipulation/array_methods.rb
+++ Blocks, collections and tests/array manipulation/array_methods.rb
@@ -11,9 +11,17 @@ def count_min_elements(arr)
   arr.count(min)
 end
 
+def sort_by_frequency(arr)
+  grouped = arr.group_by { |x| x }
+  sorted_groups = grouped.sort_by { |_, group| -group.size }
+  sorted_array = sorted_groups.flat_map { |_, group| group }
+  return sorted_array
+end
+
 arr = [-1, -1, -1, 2, 3, 4, 5]
 puts "Массив: #{arr}"
 puts "Задача 1 - #{shift_right(arr, 2)}"
 puts "Задача 2 - #{shift_right(arr, 1)}"
 puts "Задача 3 - #{count_even_elements(arr)}"
 puts "Задача 4 - #{count_min_elements(arr)}"
+puts "Задача 5 - #{sort_by_frequency(arr)}"

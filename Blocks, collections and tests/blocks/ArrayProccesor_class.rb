d2f3ae95ecb09e3dc7d6f6b5b8248897b0d1294f
pawa andreevna <80104744+eatdetey@users.noreply.github.com>
2024-10-25 21:15:46 +0300

    L3 T2.3 sort method (bubble sort)

--- Blocks, collections and tests/blocks/ArrayProccesor_class.rb
+++ Blocks, collections and tests/blocks/ArrayProccesor_class.rb
@@ -29,5 +29,21 @@ class ArrayProcessor
     end
     max_value
   end
+  
+  def sort
+    sorted = @array.dup
+    n = sorted.length
+    loop do
+      swapped = false
+      (n-1).times do |i|
+        if sorted[i] > sorted[i + 1]
+          sorted[i], sorted[i + 1] = sorted[i + 1], sorted[i]
+          swapped = true
+        end
+      end
+      break unless swapped
+    end
+    sorted
+  end
  
   end

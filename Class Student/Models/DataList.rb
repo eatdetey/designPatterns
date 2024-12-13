5241853246eba23406ac638fd705c73f7f08f60a
pawa andreevna <80104744+eatdetey@users.noreply.github.com>
2024-12-13 17:17:39 +0300

    L4 T1.4 Methods for selection added

--- Class Student/Models/DataList.rb
+++ Class Student/Models/DataList.rb
@@ -1,12 +1,35 @@
-class DataList
+class Data_List
 
     def initialize(data)
         self.data = data
+        self.selected = []
+    end
+
+    # Выбор элемента по номеру
+    def select(number)
+        unless number.is_a?(Integer) && number >= 0 && number < data.size
+            raise ArgumentError, 'The parameter must be a number within the array size'
+        end
+        self.selected << number unless self.selected.include?(number)
+    end
+
+    # Получение ID выбранных элементов
+    def get_selected
+        self.selected
+    end
+
+    def get_names
+        raise NotImplementedError, 'Method not implemented in this class'
+    end
+
+    def get_data
+        raise NotImplementedError, 'Method not implemented in this class'
     end
 
     private
 
     attr_reader :data
+    attr_accessor :selected
 
     def data=(data)
         unless data.is_a?(Array)

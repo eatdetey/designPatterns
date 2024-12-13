83e206155383eea836a5f831e588b1a46c8c2b63
pawa andreevna <80104744+eatdetey@users.noreply.github.com>
2024-12-14 00:18:25 +0300

    L4 T3.8 Tested new class

--- Class Student/Test/db_test.rb
+++ Class Student/Test/db_test.rb
@@ -1,27 +1,24 @@
 require 'pg'
+require_relative '../Models/Student/Student.rb'
+require_relative '../Models/StudentsList/StudentsListDB'
 
-begin
-  # Подключение к базе данных
-  conn = PG.connect(
-    dbname: 'students',  # Имя базы данных
-    user: 'postgres',    # Имя пользователя PostgreSQL
-    password: 'admin',  # Пароль пользователя
-    host: 'localhost',   # Хост (localhost для локальной машины)
-    port: 5434           # Порт (по умолчанию 5432)
-  )
+host = 'localhost'
+port = 5434
+dbname = 'students'
+user = 'postgres'
+password = 'admin'
 
-  # Выполнение SELECT запроса
-  result = conn.exec("SELECT * FROM student;")
+db_params = {host: host, port: port, dbname: dbname, user: user, password: password}
 
-  # Вывод результатов
-  result.each do |row|
-    puts row  # Каждая строка возвращается как хеш
-  end
+students_list_db = StudentsListDB.new(db_params)
 
-rescue PG::Error => e
-  puts "Ошибка подключения или выполнения запроса: #{e.message}"
+test_student = Student.new(surname:"Серый", name:"Максим", patronymic:"Андреевич", phone_num: "89182297016", date_of_birth: '1990-02-02')
+# students_list_db.add_student(test_student)
 
-ensure
-  # Закрытие подключения к базе данных
-  conn.close if conn
+data_list = students_list_db.get_k_n_student_short_list(1, 50)
+data = data_list.get_data
+(0..data.row_count - 1).each do |index|
+    puts "#{data.get_element(index, 0)}, #{data.get_element(index, 1)}, #{data.get_element(index, 2)}, #{data.get_element(index, 3)}"   
 end
+
+puts "Количество записей в таблице: #{students_list_db.get_student_short_count}"
